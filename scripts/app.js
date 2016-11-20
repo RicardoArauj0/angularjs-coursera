1b7fde9dbd915147f2c20b2739bfab7ad0d72f75
Ricardo Araujo <ricardofranc.araujo@gmail.com>
2016-11-20 15:02:52 -0200

    Module 2

--- scripts/app.js
+++ scripts/app.js
@@ -3,38 +3,88 @@
 
   angular.module('app', []);
 
-  angular.module('app').controller('ToMuch', CalculateToMuch);
-  CalculateToMuch.$inject=['$scope'];
+  angular.module('app')
+  .controller('ToBuy', ToBuy)
+  .controller('AlreadyBought', AlreadyBought)
+  .service('ListService', ListService);
 
-  function CalculateToMuch($scope){
-    //implemeting Jhon Papa syntaxAs style guide
+  ToBuy.$inject=['ListService'];
+  function ToBuy(ListService){
     var vm = this;
-    vm.food = "";
-    vm.message = "";
-    vm.calculate = calculate;
-    console.log(vm.food);
+    vm.buyEmpty = false;
+    vm.boughtEmpty = true;
+    
+    vm.itemsBuy = ListService.getItems(true);
 
-    function calculate(){
-      var foodsNumber = vm.food.split(",");
-      var number = foodsNumber.length;
+    vm.removeItem = function (itemIndex) {
+      vm.name = vm.itemsBuy[itemIndex].name;
+      vm.quantity = vm.itemsBuy[itemIndex].quantity;
 
-      if( number === 1 && foodsNumber[0] === ""){
-        vm.message = "Please enter data first";
-      } else if(number <= 3){
-        vm.message = "Enjoy!";
-      } else if (number > 3){
-        vm.message = "Too much!";
-      } else if ((number === 0) || (vm.food === "")){
-        vm.message = "Please enter data first";
-      } else {
-        vm.message = "Please enter data first";
+      ListService.removeItemService(itemIndex);
+
+      ListService.addItemService(vm.name, vm.quantity);
+
+      if(vm.itemsBuy.length <= 0){
+        vm.buyEmpty = true;
+      }
+
+      if (ListService.getItems(false).length >= 0) {
+        vm.boughtEmpty = false;
       }
 
     }
 
-
   }
 
+  AlreadyBought.$inject=['ListService'];
+  function AlreadyBought(ListService){
+    var vm = this;
+    vm.itemsBought = ListService.getItems(false);
+  }
 
+  function ListService() {
+    var vm = this;
+
+    // List of shopping items
+    var itemsBuy = [
+      { name: 'Cookie',
+        quantity: 10
+      },
+      { name: 'Juice',
+        quantity: 8
+      },
+      { name: 'Soda',
+        quantity: 2
+      },
+      { name: 'Tacos',
+        quantity: 15
+      },
+      { name: 'Snacks',
+        quantity: 10
+      },
+    ];
+    var itemsBought = [];
+
+    vm.addItemService = function (itemName, quantity) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      itemsBought.push(item);
+    };
+
+    vm.removeItemService = function (itemIdex) {
+      itemsBuy.splice(itemIdex, 1);
+    };
+
+    vm.getItems = function (buy) {
+      if (buy) {
+          return itemsBuy;
+      } else {
+          return itemsBought;
+      }
+
+    };
+ }
 
 })();
